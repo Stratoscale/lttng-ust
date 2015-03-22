8fc131035550cf7d521234a48e0438ab1ee32b79
yevgeny shnaidman <yevgeny@stratoscale.com>
2015-03-22 12:39:46 +0200

    fix missing processor on boot error

--- libringbuffer/smp.c
+++ libringbuffer/smp.c
@@ -22,23 +22,61 @@
 #define _LGPL_SOURCE
 #include <unistd.h>
 #include <pthread.h>
+#include <stdio.h>
 #include "smp.h"
 
 int __num_possible_cpus;
+
+
+/*
+ * parse the /sys/devices/system/cpu/possible file to get the id of maximum cpu
+ * the file of the format "number[,-number]*"
+ * we are reading the line till we reach the last number
+ */
+static int _get_max_possible_cpu_id(void)
+{
+	FILE *file;
+	int max_cpu = -1;
+
+	file = fopen("/sys/devices/system/cpu/possible", "r");
+	if (!file)
+	{
+		return -1;
+	}
+
+	for (;;) 
+	{
+		char sep;
+		int cpu;
+		int n = fscanf(file, "%u%c", &cpu, &sep);
+		if (n <= 0)
+		{
+			break;
+		}
+
+		/* condition for one last number left in line, we assume that there is no "\n" at the end of line for this file */
+		if (n == 1)
+		{
+			max_cpu = cpu;
+			break;
+		}
+	}
+
+	fclose(file);
+	return max_cpu;
+}
 
 void _get_num_possible_cpus(void)
 {
 	int result;
 
-	/* On Linux, when some processors are offline
-	 * _SC_NPROCESSORS_CONF counts the offline
-	 * processors, whereas _SC_NPROCESSORS_ONLN
-	 * does not. If we used _SC_NPROCESSORS_ONLN,
-	 * getcpu() could return a value greater than
-	 * this sysconf, in which case the arrays
-	 * indexed by processor would overflow.
-	 */
-	result = sysconf(_SC_NPROCESSORS_CONF);
+	/* On Linux , when some processor is missing from configuration ( due to some error during boot )
+ 	 * _SC_NPROCESSORS_CONF returns the total number of configured processors, and as a result
+ 	 * getcpu() could return a value greater than sysconf(_SC_NPROCESSORS_CONF). This happends in case the missing processor is not the last one.
+ 	 * therefore , we need to find the maximum defined processor, and not the number of processors defined.
+ 	 */
+	
+	result = _get_max_possible_cpu_id();
 	if (result == -1)
 		return;
 	__num_possible_cpus = result;
